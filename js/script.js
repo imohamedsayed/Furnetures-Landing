2591eec6915507255118fd7d8abc23c108fff64b
Mohamed Sayed <mohamedelsayed0901@gmailcom>
2022-11-30 03:42:17 +0200

    complete without ainmation

--- js/script.js
+++ js/script.js
@@ -10,9 +10,7 @@ navUl.forEach((li) => {
 
   sideBarUl.appendChild(copyLi);
 });
-
 // side bar toggling
-
 let navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", () => {
   sideBar.classList.toggle("open");
@@ -22,3 +20,50 @@ let closeBtn = document.querySelector(".sideBar .close");
 closeBtn.addEventListener("click", () => {
   sideBar.classList.toggle("open");
 });
+// header style
+let header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  if (scrollY > 150) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+// active nav bars
+
+let navLinks = document.querySelectorAll("header li,.sideBar li, footer li"),
+  sections = document.querySelectorAll("section");
+
+console.log(navLinks);
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navTo(link.dataset.index);
+  });
+});
+
+window.addEventListener("scroll", () => {
+  sections.forEach((sec) => {
+    let sectionOffsets = sec.getBoundingClientRect();
+
+    if (sectionOffsets.top > -300 && sectionOffsets.top <= 150) {
+      HandleActiveClasses(sec.id);
+    }
+  });
+});
+
+function HandleActiveClasses(sectionId) {
+  navLinks.forEach((a) => {
+    a.classList.remove("active");
+    if (a.dataset.index == sectionId) {
+      a.classList.add("active");
+    }
+  });
+}
+
+function navTo(sectionId) {
+  document.getElementById(`${sectionId}`).scrollIntoView({
+    behavior: "smooth",
+  });
+  closeBtn.click();
+}
